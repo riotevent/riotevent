89c8702fd0f36ad82af62fac0f9c0801b90355ff
Aurimas K <4urimas@gmail.com>
2014-06-09 20:41:37 +0200

    Add comments controller and model

---
+++ app/controllers/comments.server.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    _ = require('lodash');
+
+/**
+ * Create a Comment
+ */
+exports.create = function(req, res) {
+
+};
+
+/**
+ * Show the current Comment
+ */
+exports.read = function(req, res) {
+
+};
+
+/**
+ * Update a Comment
+ */
+exports.update = function(req, res) {
+
+};
+
+/**
+ * Delete an Comment
+ */
+exports.delete = function(req, res) {
+
+};
+
+/**
+ * List of Comments
+ */
+exports.list = function(req, res) {
+
+};
---
+++ app/models/comment.server.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Comment Schema
+ */
+var CommentSchema = new Schema({
+	// Comment model fields   
+	// ...
+});
+
+mongoose.model('Comment', CommentSchema);
