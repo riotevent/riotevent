292e44d257c87e43d22030a501f21aa4bd51813b
remi <remiclaude@free.fr>
2014-06-16 18:00:31 +0200

    Delete social connexion (facebook and shit)

--- public/application.js
+++ public/application.js
@@ -12,8 +12,6 @@ angular.module(ApplicationConfiguration.applicationModuleName).config(['$locatio
 
 //Then define the init function for starting up the application
 angular.element(document).ready(function() {
-	//Fixing facebook bug with redirect
-	if (window.location.hash === '#_=_') window.location.hash = '#!';
 
 	//Then init the app
 	angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
