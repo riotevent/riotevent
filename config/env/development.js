72b9c522f00d2d4c832d0c655a6491911ef2af0f
remi <remiclaude@free.fr>
2014-10-15 15:18:27 +0200

    Use direct transport nodemailer in dev env

--- config/env/development.js
+++ config/env/development.js
@@ -4,5 +4,9 @@ module.exports = {
 	db: 'mongodb://localhost/riotevent-dev',
 	app: {
 		title: 'riotevent - Development Environment'
-	}
+	},
+    mailer: {
+        from: process.env.MAILER_FROM || 'MAILER_FROM',
+        options: null
+    }
 };
